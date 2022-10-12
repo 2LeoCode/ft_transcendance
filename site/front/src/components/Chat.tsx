ceb0cd4405ae0e72369c9ffab1d2d488ac647377
Tiphaine DURAND <tidurand@e3r8p17.clusters.42paris.fr>
2022-10-12 19:22:46 +0200

    update css

--- site/front/src/components/Chat.tsx
+++ site/front/src/components/Chat.tsx
@@ -3,7 +3,10 @@ import '../styles/Chat.css';
 
 function Chat()
 {
-	return (<div className="Chat">Chat</div>);
+	return (
+	<div className="Chat">Chat
+		<div className='createChannel'>Create channel</div>
+	</div>);
 }
 
 export default Chat;
--- site/front/src/components/Header.tsx
+++ site/front/src/components/Header.tsx
@@ -5,7 +5,8 @@ function Header()
 {
 	return (
 	<div className='Header'>
-		<p>LeaderBoard</p>
+		<p></p>
+		<p></p>
 		<h1>PONG</h1>
 		<p>name</p>
 		<img height="50%" src="https://imgs.search.brave.com/VlAX4lbBrQGhOen-rO_KjNU3nOezhtzRZpvM6uqMrO0/rs:fit:980:966:1/g:ce/aHR0cDovL2Nkbi5v/bmxpbmV3ZWJmb250/cy5jb20vc3ZnL2lt/Z181Njg2NTcucG5n" alt="avatar" />
