94795c58e6f268dac2ec10b5c5cf5324276b0e21
tidurand <tidurand@student.42.fr>
2022-10-14 12:56:00 +0200

    front

--- site/front/src/components/Pong.tsx
+++ site/front/src/components/Pong.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import '../styles/Pong.css';
+import React from "react";
+import "../styles/Pong.css";
 
-function Pong()
-{
-	return (<div className='Pong'>Game</div>);
+function Pong() {
+  return <div className="Pong">
+	Game
+	<canvas>
+		
+	</canvas>
+	</div>;
 }
 
 export default Pong;
--- site/front/src/components/User.tsx
+++ site/front/src/components/User.tsx
@@ -11,25 +11,34 @@ declare var Blob: {
 function User() {
   const [image, setImage] = useState(new Blob());
   const [uploaded, setUploaded] = useState(false);
+  const matches_won :number = 0;
+  const matches_lost :number = 0;
 
   return (
     <div className="User">
       <h3>Username</h3>
       <div className="avatar">
-		{!uploaded && <img src="./default-avatar.webp" alt="Avatar" width="200px" />}
-        {uploaded && <img src={URL.createObjectURL(image)} alt="Avatar" width="200px" />}
-        <textarea defaultValue="test" />
+        {!uploaded && (
+          <img src="./default-avatar.webp" alt="Avatar" width="80%" />
+        )}
+        {uploaded && (
+          <img src={URL.createObjectURL(image)} alt="Avatar" width="80%" />
+        )}
+        <label htmlFor="avatar" className="avatar_label">Change avatar</label>
         <input
+          id="avatar"
+          className="avatar_button"
           type="file"
+          accept="image/*"
           onChange={(e) => {
             if (e.target.files) setImage(e.target.files[0]);
-			setUploaded(true);
+            setUploaded(true);
           }}
         />
       </div>
       <div className="stats">
-        <p>0 win</p>
-        <p>0 lose</p>
+        <p>{matches_won} win</p>
+        <p>{matches_lost} lose</p>
       </div>
       <div className="match_history">
         <p>History</p>
