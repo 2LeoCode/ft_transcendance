c07d4f204d73111f16747857a3dd498c4a4513aa
Tiphaine DURAND <tidurand@e1r8p21.clusters.42paris.fr>
2022-10-16 17:14:37 +0200

    begin chat

--- site/front/src/components/Chat.tsx
+++ site/front/src/components/Chat.tsx
@@ -7,8 +7,14 @@ function Chat() {
       <div className="left-pannel">
 		<p>Create Channel</p>
 	  </div>
-      <div className="main"></div>
-      <div className="right-pannel"></div>
+      <div className="main">
+		<div className="write_msg">
+			<p>Your Message</p>
+		</div>
+	  </div>
+      <div className="right-pannel">
+		<p>Friends</p>
+	  </div>
     </div>
   );
 }
