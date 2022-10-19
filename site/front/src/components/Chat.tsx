3afd63149acfff9ca1a2f96d2f689e3d40fe0046
Tiphaine DURAND <tidurand@e1r9p18.clusters.42paris.fr>
2022-10-19 18:42:30 +0200

    deisgn chat

--- site/front/src/components/Chat.tsx
+++ site/front/src/components/Chat.tsx
@@ -5,24 +5,30 @@ function Chat() {
   return (
     <div className="Chat">
       <div className="left-pannel">
-        <p>Create Channel</p>
-        <p>Channels</p>
+        <h3>Channels</h3>
         <ul>
           <li>Channel 1</li>
           <li>Channel 2</li>
           <li>Channel 3</li>
         </ul>
-        <p>Admins</p>
-        <p>Members</p>
-      </div>
-      <div className="main">
+        <h3>Admins</h3>
         <ul>
-          <li className="own_message">Message 1</li>
-          <li className="other_message">Message 2</li>
-          <li className="own_message">Message 3</li>
-          <li className="other_message">Message 4</li>
+          <li>Admin 1</li>
         </ul>
+        <h3>Members</h3>
+        <ul>
+          <li>Member 1</li>
+          <li>Member 2</li>
+          <li>Member 3</li>
+        </ul>
+        <h4 className="create_channel">Create Channel</h4>
       </div>
+      <ul className="main">
+        <li className="own_message">Message 1</li>
+        <li className="other_message">Message 2</li>
+        <li className="own_message">Message 3</li>
+        <li className="other_message">Message plus long 4</li>
+      </ul>
       <div className="write_msg">
         <form action="">
           <input type="text" placeholder="Message..." />
@@ -30,7 +36,12 @@ function Chat() {
         </form>
       </div>
       <div className="right-pannel">
-        <p>Friends</p>
+        <h3>Friends</h3>
+        <ul>
+          <li>Friend 1</li>
+          <li>Friend 2</li>
+          <li>Friend 3</li>
+        </ul>
       </div>
     </div>
   );
