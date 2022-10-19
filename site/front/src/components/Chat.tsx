94858d2cc2657b7453f65cc6742ca034f278409e
Tiphaine DURAND <tidurand@e3r3p17.clusters.42paris.fr>
2022-10-19 16:11:57 +0200

    html chat

--- site/front/src/components/Chat.tsx
+++ site/front/src/components/Chat.tsx
@@ -5,16 +5,33 @@ function Chat() {
   return (
     <div className="Chat">
       <div className="left-pannel">
-		<p>Create Channel</p>
-	  </div>
+        <p>Create Channel</p>
+        <p>Channels</p>
+        <ul>
+          <li>Channel 1</li>
+          <li>Channel 2</li>
+          <li>Channel 3</li>
+        </ul>
+        <p>Admins</p>
+        <p>Members</p>
+      </div>
       <div className="main">
-		<div className="write_msg">
-			<p>Your Message</p>
-		</div>
-	  </div>
+        <ul>
+          <li className="own_message">Message 1</li>
+          <li className="other_message">Message 2</li>
+          <li className="own_message">Message 3</li>
+          <li className="other_message">Message 4</li>
+        </ul>
+      </div>
+      <div className="write_msg">
+        <form action="">
+          <input type="text" placeholder="Message..." />
+          <button type="submit">Send</button>
+        </form>
+      </div>
       <div className="right-pannel">
-		<p>Friends</p>
-	  </div>
+        <p>Friends</p>
+      </div>
     </div>
   );
 }
