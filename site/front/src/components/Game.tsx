46f46db7e345d0314ca5f1cc34f2e5a86d21dcfd
Tiphaine DURAND <tidurand@e2r13p6.clusters.42paris.fr>
2022-11-17 14:17:22 +0100

    game component

--- site/front/src/components/Game.tsx
+++ site/front/src/components/Game.tsx
@@ -1,33 +1,26 @@
 import React, { useState } from "react";
 
-
 function vw_to_px(vw: number) {
-	return (window.innerWidth * vw) / 100;
-  }
-  
-  function vh_to_px(vh: number) {
-	return (window.innerHeight * vh) / 100;
-  }
+  return (window.innerWidth * vw) / 100;
+}
+
+function vh_to_px(vh: number) {
+  return (window.innerHeight * vh) / 100;
+}
 
 function Game() {
-	const [width, setWitdh] = useState<number | any>(vw_to_px(70));
-	const [height, setHeight] = useState<number | any>(vh_to_px(50));
-	window.addEventListener("resize", () => {
-		setWitdh(vw_to_px(70));
-		setHeight(vh_to_px(50));
-	  });
-	  document.addEventListener("keydown", function (e) {
-		if (e.code == "KeyW") {
-		  console.log("w pressed");
-		}
-	  });
-  return (
-    <canvas
-            id="pong"
-            width={width}
-            height={height}
-          ></canvas>
-  );
+  const [width, setWitdh] = useState<number | any>(vw_to_px(70));
+  const [height, setHeight] = useState<number | any>(vh_to_px(50));
+  window.addEventListener("resize", () => {
+    setWitdh(vw_to_px(70));
+    setHeight(vh_to_px(50));
+  });
+  document.addEventListener("keydown", function (e) {
+    if (e.code == "KeyW") {
+      console.log("w pressed");
+    }
+  });
+  return <canvas id="pong" width={width} height={height}></canvas>;
 }
 
 export default Game;
